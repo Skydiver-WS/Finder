2ca6cf3eddfd51bf9f34f5979f04da4f02070117
Skydiver-WS <93284365+Skydiver-WS@users.noreply.github.com>
2022-07-11 21:12:31 +0300

    Delete src/main/resources/Manual directory

--- src/main/resources/Manual/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"FindFileAndFolder"},{"p":"<Unnamed>","l":"Input"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"OpenResult"},{"p":"<Unnamed>","l":"Result"}];updateSearchResults();
